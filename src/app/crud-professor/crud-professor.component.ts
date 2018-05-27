7cf90f6d41a2d45cc44a6f02892ab2315b96ef75
Dionnys Santos Marinho <dionnys.santos@ulbra.edu.br>
2018-05-27 03:24:30 -0300

    Crud Professores Funcionando

--- src/app/crud-professor/crud-professor.component.ts
+++ src/app/crud-professor/crud-professor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProfessoresService} from '../professores.service';
 
 @Component({
   selector: 'app-crud-professor',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crud-professor.component.css']
 })
 export class CrudProfessorComponent implements OnInit {
+  nome = null;
+  sobrenome = null;
+  email = null;
+  telefone = null;
+  ListaProfessores = null;
 
-  constructor() { }
+  salvar_ok = null;
+  salvar_erro = null;
+  status_lista = null;
+  constructor(private professoresService: ProfessoresService) {
+    this.atualizarProfessores();
+  }
 
   ngOnInit() {
+  }
+
+  salvarProfessor() {
+    this.professoresService.salvarProfessor({
+        nome: this.nome,
+        sobrenome: this.sobrenome,
+        email: this.email,
+        telefone: this.telefone
+    }).subscribe( professor => {
+      this.salvar_ok = true;
+      alert('Professor cadastrado!');
+      this.atualizarProfessores();
+      this.limpar();
+    }, () => this.salvar_erro = true
+    );
+  }
+
+
+  atualizarProfessores() {
+    this.professoresService.todosProfessores()
+        .subscribe(
+            professores => {
+              this.ListaProfessores = professores;
+              this.status_lista = true;
+            }, () => this.status_lista = false
+        );
+  }
+
+  limpar() {
+    this.nome = null;
+    this.sobrenome = null;
+    this.email = null;
+    this.telefone = null;
   }
 
 }
--- src/app/professores.service.ts
+++ src/app/professores.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/index';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfessoresService {
+  API_URL = 'http://127.0.0.1:8000';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  todosProfessores() {
+        return this.http.get(this.API_URL + '/professores/');
+  }
+
+  salvarProfessor(professor): Observable<any> {
+        const editando = null;
+        if (editando) {
+            return this.http.patch(this.API_URL + '/professores/' + editando.id, professor);
+        } else {
+            return this.http.post(this.API_URL + '/professores/', professor);
+        }
+  }
 }
